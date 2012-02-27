a452a198dc5d9ef40126524bbaf4fcf217376485
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2012-02-27 07:56:04 +0000

    * oprava set_attrib
    git-svn-id: svn://192.168.1.145/ezer/trunk/ezer2.2@2482 c08143d5-b1a1-4836-b697-8134d2fdba83

--- client/ezer.js
+++ client/ezer.js
@@ -164,13 +164,13 @@ Ezer.Block= new Class({
 //      změní hodnotu atributu 'name' na 'val'
 //      name může být složeným jménem
 //      pokud je val objekt, bude jím nahrazena celá hodnota - narozdíl od set_attrib
-//      pokud je definováno desc bude změna provedena v popisu Block.desc, ne v instanci
+//      pokud je definováno desc bude změna provedena v popisu (ve this.desc, ne v this)
 //a: name - jméno atributu
 //   val - nová hodnota atributu
   set_attrib: function(name,val,desc) {
     Ezer.assert(typeof(name)=='string','první parametr není jménem atributu',this);
     var ids= name.split('.');
-    Ezer.assert(this.desc.part[desc],desc+" není popsáno v "+this.id,this);
+    Ezer.assert(desc==undefined || this.desc.part[desc],desc+" není popsáno v "+this.id,this);
     var o= desc==undefined ? this.options : this.desc.part[desc].options;
     for (var i= 0; i<ids.length-1; i++) {
       Ezer.assert(o[ids[i]],name+" je chybné jméno v set_attrib",this);
